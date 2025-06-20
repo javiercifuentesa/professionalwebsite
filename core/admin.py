0bb37d70e4dc2d34b1c81989688259de6d9b8667
javiercifuentesa <javier.cifuentesa@icloud.com>
2025-06-20 18:36:29 +0200

    deploy update admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.site_header = "JaviCifuentesA's Panel Admin"
+admin.site.site_title = "JaviCifuentesA's Panel Admin"
+admin.site.index_title = "JaviCifuentesA's Panel Admin"
