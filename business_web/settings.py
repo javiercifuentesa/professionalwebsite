062fa89cb9879f7b30897c43107af124924da3f7
javiercifuentesa <javier.cifuentesa@icloud.com>
2025-06-21 11:20:17 +0200

    cloudinary3

--- business_web/settings.py
+++ business_web/settings.py
@@ -135,14 +135,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # media config
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Cloudinary config
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME', 'dssbwvdd5'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY', '359874328174144'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET', 'davShAQ90122-7ISOrgD_LcAUPs'),
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 # Cloudinary storage
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
