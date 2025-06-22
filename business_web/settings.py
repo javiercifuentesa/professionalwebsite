8ec995dbf95dd47635c4df805435a30930a3e15f
javiercifuentesa <javier.cifuentesa@icloud.com>
2025-06-22 14:32:37 +0200

    pythonanywhere deploy2

--- business_web/settings.py
+++ business_web/settings.py
@@ -85,12 +85,19 @@ WSGI_APPLICATION = 'business_web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.environ.get('DATABASE_URL')
+#     )
+# }
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
